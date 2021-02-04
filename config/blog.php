99fec538bc8f1567a5414f2f711fb9ecf4f677ee
Wolfred Montilla <wolfrednicolas@gmail.com>
2021-02-04 17:43:19 -0500

    add classes required in the config file

--- config/blog.php
+++ config/blog.php
@@ -1,5 +1,15 @@
 <?php
 
 return [
+    /**
+     * Model class references
+     */
+    'model_class' => [
 
+        'page' => \App\Models\Page::class,
+        'post' => \App\Models\Post::class,
+        'series' => \App\Models\Series::class,
+        'topic' => \App\Models\Topic::class
+
+    ]
 ];

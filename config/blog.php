79ac4213dc8b654ab68436760d7a57bbc98c699b
Drew Roberts <github@drewroberts.com>
2021-02-05 11:48:14 -0500

    Revert config file to use tipoff support config

--- config/blog.php
+++ config/blog.php
@@ -1,15 +1,5 @@
 <?php
 
 return [
-    /**
-     * Model class references
-     */
-    'model_class' => [
-
-        'page' => \App\Models\Page::class,
-        'post' => \App\Models\Post::class,
-        'series' => \App\Models\Series::class,
-        'topic' => \App\Models\Topic::class
-
-    ]
+    
 ];

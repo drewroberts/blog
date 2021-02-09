518d6a2408e4a747ce376fb572ab999eef515694
drewroberts <github@drewroberts.com>
2021-02-09 14:30:18 -0500

    Refacotr migrations with foreignIdFor()

--- database/migrations/2017_05_20_120000_create_posts_table.php
+++ database/migrations/2017_05_20_120000_create_posts_table.php
@@ -17,15 +17,14 @@ class CreatePostsTable extends Migration
             $table->string('ogdescription')->nullable(); // Open Graph Description used for social shares. Will default to description if NULL.
             $table->unsignedInteger('pageviews')->index(); // Total current pageviews for post. Will be synced from Google Analytics API.
 
-            $table->foreignId('topic_id')->nullable()->references('id')->on('topics'); // Group blog posts into topics
-            $table->foreignId('series_id')->nullable()->references('id')->on('series'); // Group blog posts into series
-            $table->foreignId('image_id')->nullable()->references('id')->on('images'); // Cover image for post
-            $table->foreignId('ogimage_id')->nullable()->references('id')->on('images'); // External open graph image id. Featured image for social sharing. Will default to image_id unless this is used. Allows override for play button or words on image.
-            $table->foreignId('video_id')->nullable()->references('id')->on('videos'); // If blog post has a featured video.
+            $table->foreignIdFor(app('topic'))->nullable(); // Group blog posts into topics
+            $table->foreignIdFor(app('image'))->nullable(); // Group blog posts into series
+            $table->foreignIdFor(app('image'), 'ogimage_id')->nullable(); // External open graph image id. Featured image for social sharing. Will default to image_id unless this is used. Allows override for play button or words on image.
+            $table->foreignIdFor(app('video'))->nullable(); // If blog post has a featured video.
 
-            $table->foreignId('author_id')->references('id')->on('users'); // Author of the post.
-            $table->foreignId('creator_id')->references('id')->on('users');
-            $table->foreignId('updater_id')->references('id')->on('users');
+            $table->foreignIdFor(app('user'), 'author_id'); // Author of the post.
+            $table->foreignIdFor(app('user'), 'creator_id');
+            $table->foreignIdFor(app('user'), 'updater_id');
 
             $table->dateTime('published_at'); // Allows blog posts to be published at a later date.
             $table->softDeletes();

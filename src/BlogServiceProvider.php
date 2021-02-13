7e3ebc6ca87b660f71331329f7c0f6efa689c7c0
auxiliawebsitedesign <auxiliawebsitedesign@users.noreply.github.com>
2021-02-13 23:20:10 +0000

    Fix styling

--- src/BlogServiceProvider.php
+++ src/BlogServiceProvider.php
@@ -4,10 +4,6 @@ declare(strict_types=1);
 
 namespace DrewRoberts\Blog;
 
-use Tipoff\Blog\Models\Page;
-use Tipoff\Blog\Models\Post;
-use Tipoff\Blog\Models\Series;
-use Tipoff\Blog\Models\Topic;
 use Tipoff\Blog\Policies\BlogPolicy;
 use Tipoff\Support\TipoffPackage;
 use Tipoff\Support\TipoffServiceProvider;

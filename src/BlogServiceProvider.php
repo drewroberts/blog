c320c82f1e1bb027ba3df69bc3191b9acd03d96a
drewroberts <drewroberts@users.noreply.github.com>
2021-02-14 12:34:48 +0000

    Fix styling

--- src/BlogServiceProvider.php
+++ src/BlogServiceProvider.php
@@ -4,10 +4,6 @@ declare(strict_types=1);
 
 namespace DrewRoberts\Blog;
 
-use Tipoff\Blog\Models\Page;
-use Tipoff\Blog\Models\Post;
-use Tipoff\Blog\Models\Series;
-use Tipoff\Blog\Models\Topic;
 use Tipoff\Support\TipoffPackage;
 use Tipoff\Support\TipoffServiceProvider;
 

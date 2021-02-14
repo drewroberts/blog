074fd84b9dcf2bec9c4ab563ed5f13d699d88042
Drew Roberts <github@drewroberts.com>
2021-02-14 07:34:14 -0500

    Import models

--- src/BlogServiceProvider.php
+++ src/BlogServiceProvider.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace DrewRoberts\Blog;
 
-use Tipoff\Blog\Policies\BlogPolicy;
+use Tipoff\Blog\Models\Page;
+use Tipoff\Blog\Models\Post;
+use Tipoff\Blog\Models\Series;
+use Tipoff\Blog\Models\Topic;
 use Tipoff\Support\TipoffPackage;
 use Tipoff\Support\TipoffServiceProvider;
 
@@ -13,9 +16,6 @@ class BlogServiceProvider extends TipoffServiceProvider
     public function configureTipoffPackage(TipoffPackage $package): void
     {
         $package
-            ->hasPolicies([
-                Blog::class => BlogPolicy::class,
-            ])
             ->name('blog')
             ->hasConfigFile();
     }

315eb97c7b5cb81b21ad5ab0232edc7e4ef779c1
Gustavo Gentil <gustavorgentil@outlook.com>
2021-02-17 16:56:52 -0300

    Register policies in service provider

--- src/BlogServiceProvider.php
+++ src/BlogServiceProvider.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace DrewRoberts\Blog;
 
+use DrewRoberts\Blog\Models\Page;
+use DrewRoberts\Blog\Policies\PagePolicy;
 use Tipoff\Support\TipoffPackage;
 use Tipoff\Support\TipoffServiceProvider;
 
@@ -12,6 +14,9 @@ class BlogServiceProvider extends TipoffServiceProvider
     public function configureTipoffPackage(TipoffPackage $package): void
     {
         $package
+            ->hasPolicies([
+                Page::class => PagePolicy::class,
+            ])
             ->name('blog')
             ->hasConfigFile();
     }

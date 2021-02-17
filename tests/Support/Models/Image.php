092d1004de5e23d717ba8f164aa957cab596f5d7
Gustavo Gentil <gustavorgentil@outlook.com>
2021-02-17 16:01:38 -0300

    Set up TestCase with test doubles

---
+++ tests/Support/Models/Image.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace DrewRoberts\Blog\Tests\Support\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Tipoff\Support\Models\TestModelStub;
+
+class Image extends Model
+{
+    use TestModelStub;
+}

4ae0f718e902ff5ae0c0389f662f3caaa5755fa7
Drew Roberts <github@drewroberts.com>
2021-02-22 09:58:49 -0500

    Create topic policy

---
+++ src/Policies/TopicPolicy.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace DrewRoberts\Blog\Policies;
+
+use DrewRoberts\Blog\Models\Topic;
+use Illuminate\Auth\Access\HandlesAuthorization;
+use Tipoff\Support\Contracts\Models\UserInterface;
+
+class PagePolicy
+{
+    use HandlesAuthorization;
+
+    public function viewAny(UserInterface $user)
+    {
+        return $user->hasPermissionTo('view topics');
+    }
+
+    public function view(UserInterface $user, Topic $topic)
+    {
+        return $user->hasPermissionTo('view topics');
+    }
+
+    public function create(UserInterface $user)
+    {
+        return $user->hasPermissionTo('create topics');
+    }
+
+    public function update(UserInterface $user, Topic $topic)
+    {
+        return $user->hasPermissionTo('update topics');
+    }
+    
+    public function delete(UserInterface $user, Topic $topic)
+    {
+        return false;
+    }
+
+    public function restore(UserInterface $user, Topic $topic)
+    {
+        return false;
+    }
+
+    public function forceDelete(UserInterface $user, Topic $topic)
+    {
+        return false;
+    }
+}

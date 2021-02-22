2ce1f0bb2af3ea19fb81fcb07af9c93d94818f3d
Drew Roberts <github@drewroberts.com>
2021-02-22 10:03:35 -0500

    Register policies

--- src/BlogServiceProvider.php
+++ src/BlogServiceProvider.php
@@ -5,7 +5,13 @@ declare(strict_types=1);
 namespace DrewRoberts\Blog;
 
 use DrewRoberts\Blog\Models\Page;
+use DrewRoberts\Blog\Models\Post;
+use DrewRoberts\Blog\Models\Series;
+use DrewRoberts\Blog\Models\Topic;
 use DrewRoberts\Blog\Policies\PagePolicy;
+use DrewRoberts\Blog\Policies\PostPolicy;
+use DrewRoberts\Blog\Policies\SeriesPolicy;
+use DrewRoberts\Blog\Policies\TopicPolicy;
 use Tipoff\Support\TipoffPackage;
 use Tipoff\Support\TipoffServiceProvider;
 
@@ -16,6 +22,9 @@ class BlogServiceProvider extends TipoffServiceProvider
         $package
             ->hasPolicies([
                 Page::class => PagePolicy::class,
+                Post::class => PostPolicy::class,
+                Series::class => SeriesPolicy::class,
+                Topic::class => TopicPolicy::class,
             ])
             ->hasNovaResources([
                 \DrewRoberts\Blog\Nova\Page::class,

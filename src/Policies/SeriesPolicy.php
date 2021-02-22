c533c01cf152b74f2500861e6186fd0730739327
Drew Roberts <github@drewroberts.com>
2021-02-22 09:55:51 -0500

    Create series policy

---
+++ src/Policies/SeriesPolicy.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace DrewRoberts\Blog\Policies;
+
+use DrewRoberts\Blog\Models\Series;
+use Illuminate\Auth\Access\HandlesAuthorization;
+use Tipoff\Support\Contracts\Models\UserInterface;
+
+class PagePolicy
+{
+    use HandlesAuthorization;
+
+    public function viewAny(UserInterface $user)
+    {
+        return $user->hasPermissionTo('view series');
+    }
+
+    public function view(UserInterface $user, Series $series)
+    {
+        return $user->hasPermissionTo('view series');
+    }
+
+    public function create(UserInterface $user)
+    {
+        return $user->hasPermissionTo('create series');
+    }
+
+    public function update(UserInterface $user, Series $series)
+    {
+        return $user->hasPermissionTo('update series');
+    }
+    
+    public function delete(UserInterface $user, Series $series)	
+    {	
+        return false;	
+    }	
+
+    public function restore(UserInterface $user, Series $series)	
+    {	
+        return false;	
+    }	
+
+    public function forceDelete(UserInterface $user, Series $series)	
+    {	
+        return false;	
+    }
+}

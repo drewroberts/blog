4e24f89918ac93e5d2d466c02b8d43e7bd1b7c2d
Drew Roberts <github@drewroberts.com>
2021-03-17 10:22:19 -0400

    Use nova class helper to reference other resources

--- src/Nova/Page.php
+++ src/Nova/Page.php
@@ -33,7 +33,7 @@ class Page extends BaseResource
             ID::make()->sortable(),
             Text::make('Slug')->sortable(),
             Text::make('Title')->sortable(),
-            BelongsTo::make('Parent', 'parent', \DrewRoberts\Blog\Nova\Page::class)->sortable(),
+            nova('page') ? BelongsTo::make('Parent', 'parent', nova('page'))->sortable() : null,
             BelongsTo::make('Author', 'author', nova('user'))->sortable(),
             DateTime::make('Published', 'published_at')->format('YYYY-MM-DD')->sortable(),
         ];
@@ -45,7 +45,7 @@ class Page extends BaseResource
             Text::make('Title')->required(),
             Slug::make('Slug')->from('Title'),
             DateTime::make('Published', 'published_at'),
-            BelongsTo::make('Parent', 'parent', \DrewRoberts\Blog\Nova\Page::class)->nullable(),
+            nova('page') ? BelongsTo::make('Parent', 'parent', nova('page'))->nullable() : null,
             Markdown::make('Content')->help(
                 '<a href="https://www.markdownguide.org">MarkdownGuide.org</a>'
             )->stacked(),
@@ -61,9 +61,9 @@ class Page extends BaseResource
             BelongsTo::make('Author', 'author', app('nova.user'))->nullable(),
             Textarea::make('Description'),
             Textarea::make('Open Graph Description', 'ogdescription')->nullable(),
-            BelongsTo::make('Image', 'image', \DrewRoberts\Media\Nova\Image::class)->nullable()->showCreateRelationButton(),
-            BelongsTo::make('OG Image', 'ogimage', \DrewRoberts\Media\Nova\Image::class)->nullable()->showCreateRelationButton(),
-            BelongsTo::make('Video', 'video', \DrewRoberts\Media\Nova\Video::class)->nullable()->showCreateRelationButton(),
+            nova('image') ? BelongsTo::make('Image', 'image', nova('image'))->nullable()->showCreateRelationButton() : null,
+            nova('image') ? BelongsTo::make('OG Image', 'ogimage', nova('image'))->nullable()->showCreateRelationButton() : null,
+            nova('video') ? BelongsTo::make('Video', 'video', nova('video'))->nullable()->showCreateRelationButton() : null,
         ];
     }
 

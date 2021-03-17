d6013fe1534ea7762eb1d33324f140b04b9a8335
Drew Roberts <github@drewroberts.com>
2021-03-17 09:58:16 -0400

    Update data fields on page nova resource #15

--- src/Nova/Page.php
+++ src/Nova/Page.php
@@ -12,9 +12,9 @@ use Laravel\Nova\Fields\Text;
 use Laravel\Nova\Fields\Textarea;
 use Laravel\Nova\Http\Requests\NovaRequest;
 use Laravel\Nova\Panel;
-use Laravel\Nova\Resource;
+use Tipoff\Support\Nova\BaseResource;
 
-class Page extends Resource
+class Page extends BaseResource
 {
     public static $model = \DrewRoberts\Blog\Models\Page::class;
 
@@ -67,39 +67,12 @@ class Page extends Resource
         ];
     }
 
-    protected function dataFields()
+    protected function dataFields(): array
     {
-        return [
-            ID::make(),
-            BelongsTo::make('Created By', 'creator', app('nova.user'))->exceptOnForms(),
-            DateTime::make('Created At')->exceptOnForms(),
-            BelongsTo::make('Updated By', 'updater', app('nova.user'))->exceptOnForms(),
-            DateTime::make('Updated At')->exceptOnForms(),
-        ];
-    }
-
-    public function cards(Request $request)
-    {
-        return [];
-    }
-
-    public function filters(Request $request)
-    {
-        return [];
-    }
-
-    public function lenses(Request $request)
-    {
-        return [];
-    }
-
-    public function actions(Request $request)
-    {
-        return [];
-    }
-
-    public function authorizedToForceDelete(Request $request)
-    {
-        return false;
+        return array_merge(
+            parent::dataFields(),
+            $this->creatorDataFields(),
+            $this->updaterDataFields(),
+        );
     }
 }

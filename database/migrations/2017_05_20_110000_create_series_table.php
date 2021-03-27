a0b2e5d5865a4c44f511b5ed358b5ecbf5be0b24
Drew Roberts <github@drewroberts.com>
2021-03-27 12:43:29 -0400

    Add webpage to series migration #56 #4

--- database/migrations/2017_05_20_110000_create_series_table.php
+++ database/migrations/2017_05_20_110000_create_series_table.php
@@ -10,15 +10,17 @@ class CreateSeriesTable extends Migration
     {
         Schema::create('series', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(app('topic')); // Group series into larger topics
             $table->string('slug')->unique()->index();
             $table->string('title')->unique();
             $table->string('note')->nullable(); // Just for internal reference purposes only, not displayed on website.
             $table->text('content')->nullable(); // Will be written in Markdown.
-            $table->string('description')->nullable(); // Primary description used for SEO.
-            $table->string('ogdescription')->nullable(); // Open Graph Description used for social shares. Will default to description if NULL.
+
+            $table->foreignIdFor(app('webpage'))->nullable(); // Used to track seo rankings
             $table->unsignedInteger('pageviews')->index(); // Total current pageviews for series page. Will be synced from Google Analytics API.
 
-            $table->foreignIdFor(app('topic')); // Group series into larger topics
+            $table->string('description')->nullable(); // Primary description used for SEO.
+            $table->string('ogdescription')->nullable(); // Open Graph Description used for social shares. Will default to description if NULL.
             $table->foreignIdFor(app('image'))->nullable(); // Cover image for series
             $table->foreignIdFor(app('image'), 'ogimage_id')->nullable(); // External open graph image id. Featured image for social sharing. Will default to image_id unless this is used. Allows override for play button or words on image.
             $table->foreignIdFor(app('video'))->nullable(); // If series has a featured video.

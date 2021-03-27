f860033244ceada84a67b784ad454f52773cef82
Drew Roberts <github@drewroberts.com>
2021-03-27 12:45:28 -0400

    Add webpage to post migration #56 #5

--- database/migrations/2017_05_20_120000_create_posts_table.php
+++ database/migrations/2017_05_20_120000_create_posts_table.php
@@ -12,13 +12,15 @@ class CreatePostsTable extends Migration
             $table->id();
             $table->string('slug')->unique()->index();
             $table->string('title')->unique();
-            $table->text('content')->nullable(); // Will be written in Markdown.
-            $table->string('description')->nullable(); // Primary description used for SEO.
-            $table->string('ogdescription')->nullable(); // Open Graph Description used for social shares. Will default to description if NULL.
-            $table->unsignedInteger('pageviews')->index(); // Total current pageviews for post. Will be synced from Google Analytics API.
-
             $table->foreignIdFor(app('topic'))->nullable(); // Group blog posts into topics
             $table->foreignIdFor(app('series'))->nullable(); // Group blog posts into series
+            $table->text('content')->nullable(); // Will be written in Markdown.
+
+            $table->foreignIdFor(app('webpage'))->nullable(); // Used to track seo rankings
+            $table->unsignedInteger('pageviews')->index(); // Total current pageviews for post. Will be synced from Google Analytics API.
+
+            $table->string('description')->nullable(); // Primary description used for SEO.
+            $table->string('ogdescription')->nullable(); // Open Graph Description used for social shares. Will default to description if NULL.
             $table->foreignIdFor(app('image'))->nullable(); // Cover image for blog post
             $table->foreignIdFor(app('image'), 'ogimage_id')->nullable(); // External open graph image id. Featured image for social sharing. Will default to image_id unless this is used. Allows override for play button or words on image.
             $table->foreignIdFor(app('video'))->nullable(); // If blog post has a featured video.

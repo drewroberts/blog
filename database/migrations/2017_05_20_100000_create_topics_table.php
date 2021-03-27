68b76dddc8de4984f8959b1517d88f19c9281082
Drew Roberts <github@drewroberts.com>
2021-03-27 12:41:14 -0400

    Add webpage to topic migration #56 #3

--- database/migrations/2017_05_20_100000_create_topics_table.php
+++ database/migrations/2017_05_20_100000_create_topics_table.php
@@ -14,10 +14,12 @@ class CreateTopicsTable extends Migration
             $table->string('title')->unique();
             $table->string('note')->nullable(); // Just for internal reference purposes only, not displayed on website.
             $table->text('content')->nullable(); // Will be written in Markdown.
+            
+            $table->foreignIdFor(app('webpage'))->nullable(); // Used to track seo rankings
+            $table->unsignedInteger('pageviews')->index(); // Total current pageviews for topic page. Will be synced from Google Analytics API.
+            
             $table->string('description')->nullable(); // Primary description used for SEO.
             $table->string('ogdescription')->nullable(); // Open Graph Description used for social shares. Will default to description if NULL.
-            $table->unsignedInteger('pageviews')->index(); // Total current pageviews for topic page. Will be synced from Google Analytics API.
-
             $table->foreignIdFor(app('image'))->nullable(); // Cover image for topic
             $table->foreignIdFor(app('image'), 'ogimage_id')->nullable(); // External open graph image id. Featured image for social sharing. Will default to image_id unless this is used. Allows override for play button or words on image.
             $table->foreignIdFor(app('video'))->nullable(); // If topic has a featured video.

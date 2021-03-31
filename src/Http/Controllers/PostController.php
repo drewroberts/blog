0adc89f22cc974420031401d57be655666354ad9
Wolfred Montilla <wolfrednicolas@gmail.com>
2021-03-31 15:05:32 -0400

    update PostController

--- src/Http/Controllers/PostController.php
+++ src/Http/Controllers/PostController.php
@@ -5,13 +5,19 @@ declare(strict_types=1);
 namespace DrewRoberts\Blog\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DrewRoberts\Blog\Models\Topic;
+use DrewRoberts\Blog\Models\Series;
 use DrewRoberts\Blog\Models\Post;
 use Tipoff\Support\Http\Controllers\BaseController;
 
 class PostController extends BaseController
 {
-    public function __invoke(Request $request, Post $post)
+    public function __invoke(Request $request, Topic $topic, Series $series, Post $post)
     {
-        return redirect('/');
+        return view('blog::post', [
+            'topic' => $topic,
+            'series' => $series,
+            'post' => $post
+        ]);
     }
 }

1ea05d05a9b2cebfef25e965ccecd701df04e9c5
Wolfred Montilla <wolfrednicolas@gmail.com>
2021-03-31 15:07:20 -0400

    update PageControllerTest

--- tests/Unit/Http/Controllers/PageControllerTest.php
+++ tests/Unit/Http/Controllers/PageControllerTest.php
@@ -16,7 +16,6 @@ class PageControllerTest extends TestCase
     /** @test */
     public function index_single_page()
     {
-
         $page = Page::factory()->create();
 
         $child_page = Page::factory()->create();
@@ -25,12 +24,10 @@ class PageControllerTest extends TestCase
         $grand_child_page = Page::factory()->create();
         $grand_child_page->setParent($child_page);
 
-        $this->get($this->webUrl("{$page->slug}/{$child_page->slug}/{$grand_child_page->slug}"))
+        $this->get($this->webUrl("/blog/{$page->slug}/{$child_page->slug}/{$grand_child_page->slug}"))
             ->assertOk()
             ->assertSee($page->name)
             ->assertSee($child_page->name)
             ->assertSee($grand_child_page->name);
     }
-
-
 }

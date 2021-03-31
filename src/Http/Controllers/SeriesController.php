d978a6186ef3bb9783026f0a7c5cd18129fa7dcf
Wolfred Montilla <wolfrednicolas@gmail.com>
2021-03-31 15:05:43 -0400

    update SeriesController

--- src/Http/Controllers/SeriesController.php
+++ src/Http/Controllers/SeriesController.php
@@ -6,12 +6,16 @@ namespace DrewRoberts\Blog\Http\Controllers;
 
 use Illuminate\Http\Request;
 use DrewRoberts\Blog\Models\Series;
+use DrewRoberts\Blog\Models\Topic;
 use Tipoff\Support\Http\Controllers\BaseController;
 
 class SeriesController extends BaseController
 {
-    public function __invoke(Request $request, Series $series)
+    public function __invoke(Request $request, Topic $topic, Series $series)
     {
-        return redirect('/');
+        return view('blog::series', [
+            'topic' => $topic,
+            'series' => $series
+        ]);
     }
 }

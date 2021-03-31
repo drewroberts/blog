329fa7689f4392fbd4dee20f221bc17e07eefab6
Wolfred Montilla <wolfrednicolas@gmail.com>
2021-03-31 14:58:47 -0400

    fix routes

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,19 @@
 use Illuminate\Support\Facades\Route;
 use DrewRoberts\Blog\Http\Controllers\PageController;
 use DrewRoberts\Blog\Http\Controllers\TopicController;
+use DrewRoberts\Blog\Http\Controllers\SeriesController;
+use DrewRoberts\Blog\Http\Controllers\PostController;
 
 
 Route::middleware(config('tipoff.web.middleware_group'))
     ->prefix(config('tipoff.web.uri_prefix'))
     ->group(function () {
 
-        Route::get('{page}/{child_page}/{grand_child_page}', PageController::class)
+        Route::get('/blog/{page}/{child_page}/{grand_child_page}', PageController::class)
             ->name('page');
 
-        Route::get('{topic}/{series}/{post}', TopicController::class)
-            ->name('topic');
+        Route::get('/blog/{topic}', TopicController::class)->name('topic');
+		Route::get('/blog/{topic}/{series}', SeriesController::class)->name('series');
+		Route::get('/blog/{topic}/{series}/{post}', PostController::class)->name('post');
 
     });

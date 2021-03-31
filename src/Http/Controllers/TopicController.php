b848b765c19f64e9bcfb6966ec883059f36d24b6
Wolfred Montilla <wolfrednicolas@gmail.com>
2021-03-31 15:06:30 -0400

    update TopicController

--- src/Http/Controllers/TopicController.php
+++ src/Http/Controllers/TopicController.php
@@ -6,12 +6,16 @@ namespace DrewRoberts\Blog\Http\Controllers;
 
 use Illuminate\Http\Request;
 use DrewRoberts\Blog\Models\Topic;
+use DrewRoberts\Blog\Models\Series;
+use DrewRoberts\Blog\Models\Post;
 use Tipoff\Support\Http\Controllers\BaseController;
 
 class TopicController extends BaseController
 {
     public function __invoke(Request $request, Topic $topic)
     {
-        return redirect('/');
+        return view('blog::topic', [
+            'topic' => $topic,
+        ]);
     }
 }

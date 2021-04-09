db97352675a79433a1bc592e5cb8c56f755dc78e
Wolfred Montilla <wolfrednicolas@gmail.com>
2021-04-09 19:06:23 -0400

    fix issue

---
+++ src/Nova/Layout.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace DrewRoberts\Blog\Nova;
+
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\BelongsTo;
+use Laravel\Nova\Fields\DateTime;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Markdown;
+use Laravel\Nova\Fields\Slug;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Fields\Textarea;
+use Laravel\Nova\Http\Requests\NovaRequest;
+use Laravel\Nova\Panel;
+use Tipoff\Support\Nova\BaseResource;
+
+class Layout extends BaseResource
+{
+    public static $model = \DrewRoberts\Blog\Models\Layout::class;
+
+    public static $title = 'name';
+
+    public static $search = [
+        'id',
+        'name',
+    ];
+
+    public static $group = 'Website Content';
+
+    public function fieldsForIndex(NovaRequest $request)
+    {
+        return [
+            ID::make()->sortable(),
+            Text::make('Slug')->sortable(),
+            Text::make('Title')->sortable(),
+            nova('page') ? BelongsTo::make('Page', 'page', nova('page'))->sortable() : null,
+            nova('user') ? BelongsTo::make('Author', 'author', nova('user'))->sortable() : null,
+            DateTime::make('Published', 'published_at')->format('YYYY-MM-DD')->sortable(),
+        ];
+    }
+
+    public function fields(Request $request)
+    {
+        return [
+            Text::make('Title')->required(),
+            Slug::make('Slug')->from('Title'),
+            DateTime::make('Published', 'published_at'),
+            nova('page') ? BelongsTo::make('Parent', 'parent', nova('page'))->nullable() : null,
+            Markdown::make('Content')->help(
+                '<a href="https://www.markdownguide.org">MarkdownGuide.org</a>'
+            )->stacked(),
+
+            new Panel('Info Fields', $this->infoFields()),
+            new Panel('Data Fields', $this->dataFields()),
+        ];
+    }
+
+    protected function infoFields()
+    {
+        return [
+            nova('user') ? BelongsTo::make('Author', 'author', nova('user'))->nullable() : null,
+            Textarea::make('Description'),
+            Textarea::make('Open Graph Description', 'ogdescription')->nullable(),
+            nova('image') ? BelongsTo::make('Image', 'image', nova('image'))->nullable()->showCreateRelationButton() : null,
+            nova('image') ? BelongsTo::make('OG Image', 'ogimage', nova('image'))->nullable()->showCreateRelationButton() : null,
+            nova('video') ? BelongsTo::make('Video', 'video', nova('video'))->nullable()->showCreateRelationButton() : null,
+        ];
+    }
+
+    protected function dataFields(): array
+    {
+        return array_merge(
+            parent::dataFields(),
+            $this->creatorDataFields(),
+            $this->updaterDataFields(),
+        );
+    }
+}

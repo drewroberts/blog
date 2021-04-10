8e454497ff5e021a37e2a83b8a3120783d230151
drewroberts <drewroberts@users.noreply.github.com>
2021-04-10 00:26:50 +0000

    Fix styling

--- src/Nova/Layout.php
+++ src/Nova/Layout.php
@@ -4,7 +4,6 @@ namespace DrewRoberts\Blog\Nova;
 
 use Illuminate\Http\Request;
 use Laravel\Nova\Fields\BelongsTo;
-use Laravel\Nova\Fields\HasMany;
 use Laravel\Nova\Fields\ID;
 use Laravel\Nova\Fields\Text;
 use Laravel\Nova\Http\Requests\NovaRequest;

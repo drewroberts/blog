38e228ae0c51004b81befc2e82152d45c40a15b4
Drew Roberts <github@drewroberts.com>
2021-04-09 20:16:34 -0400

    Add layout relationship to page resource

--- src/Nova/Page.php
+++ src/Nova/Page.php
@@ -35,7 +35,6 @@ class Page extends BaseResource
             Text::make('Title')->sortable(),
             nova('page') ? BelongsTo::make('Parent', 'parent', nova('page'))->sortable() : null,
             nova('user') ? BelongsTo::make('Author', 'author', nova('user'))->sortable() : null,
-            nova('layout') ? BelongsTo::make('Layout', 'layout', nova('layout'))->nullable() : null,
             DateTime::make('Published', 'published_at')->format('YYYY-MM-DD')->sortable(),
         ];
     }
@@ -45,6 +44,7 @@ class Page extends BaseResource
         return [
             Text::make('Title')->required(),
             Slug::make('Slug')->from('Title'),
+            nova('layout') ? BelongsTo::make('Layout', 'layout', nova('layout'))->nullable() : null,
             DateTime::make('Published', 'published_at'),
             nova('page') ? BelongsTo::make('Parent', 'parent', nova('page'))->nullable() : null,
             Markdown::make('Content')->help(

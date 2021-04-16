327c2fbb44b39db35443878cdadb57cfba1fd655
Wolfred Montilla <wolfrednicolas@gmail.com>
2021-04-16 16:07:12 -0400

    add relatableLayouts function to filter properly

--- src/Nova/Topic.php
+++ src/Nova/Topic.php
@@ -13,6 +13,7 @@ use Laravel\Nova\Fields\Textarea;
 use Laravel\Nova\Http\Requests\NovaRequest;
 use Laravel\Nova\Panel;
 use Tipoff\Support\Nova\BaseResource;
+use Tipoff\Support\Enums\LayoutType;
 
 class Topic extends BaseResource
 {
@@ -26,6 +27,11 @@ class Topic extends BaseResource
     ];
 
     public static $group = 'Website Blog';
+
+    public static function relatableLayouts(NovaRequest $request, $query)
+    {
+        return $query->where('layout_type', LayoutType::TOPIC);
+    }
 
     public function fieldsForIndex(NovaRequest $request)
     {

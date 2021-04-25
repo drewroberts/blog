7362202f9beffba0bd2420e103d1ce43efe51569
Wolfred Montilla <wolfrednicolas@gmail.com>
2021-04-25 13:50:05 -0400

    rollback change

--- src/Nova/Actions/PreviewPost.php
+++ src/Nova/Actions/PreviewPost.php
@@ -23,7 +23,7 @@ class PreviewPost extends Action
      */
     public function handle(ActionFields $fields, Collection $models)
     {
-        return Action::openInNewTab(config('app.url') . "/".config('tipoff.web.uri_prefix') . $models->first()->path);
+        return Action::openInNewTab(config('app.url') . config('tipoff.web.uri_prefix') . $models->first()->path);
     }
 
     /**

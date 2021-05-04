c3750ed1a4e0d4b132d73aabcd5615e5c9a4517a
chx2 <chx2@users.noreply.github.com>
2021-05-04 23:40:36 +0000

    Fix styling

--- src/Nova/Series.php
+++ src/Nova/Series.php
@@ -2,6 +2,7 @@
 
 namespace DrewRoberts\Blog\Nova;
 
+use DrewRoberts\Blog\Nova\Fields\TextCopy;
 use Illuminate\Http\Request;
 use Laravel\Nova\Fields\BelongsTo;
 use Laravel\Nova\Fields\HasMany;
@@ -12,7 +13,6 @@ use Laravel\Nova\Fields\Text;
 use Laravel\Nova\Fields\Textarea;
 use Laravel\Nova\Http\Requests\NovaRequest;
 use Laravel\Nova\Panel;
-use DrewRoberts\Blog\Nova\Fields\TextCopy;
 use Tipoff\Support\Enums\LayoutType;
 use Tipoff\Support\Nova\BaseResource;
 

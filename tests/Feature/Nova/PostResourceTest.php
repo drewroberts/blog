b26b61c901ba8b865212de4cf376aa5605a17566
ryzhykE <eugene@buildmidwestern.com>
2021-05-17 15:21:36 +0300

    Create relatableLayouts test for PostResource #17

--- tests/Feature/Nova/PostResourceTest.php
+++ tests/Feature/Nova/PostResourceTest.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace DrewRoberts\Blog\Tests\Feature\Nova;
 
+use DrewRoberts\Blog\Models\Layout;
 use DrewRoberts\Blog\Models\Post;
 use DrewRoberts\Blog\Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Laravel\Nova\Http\Requests\NovaRequest;
 use Tipoff\Authorization\Models\User;
 
 class PostResourceTest extends TestCase
@@ -38,5 +40,20 @@ class PostResourceTest extends TestCase
             ->assertOk();
 
         $this->assertEquals($post->id, $response->json('resource.id.value'));
+    }
+
+    /** @test */
+    public function relatableLayouts()
+    {
+        $layout = Layout::factory()->create();
+        $posts = Post::factory()->create()->layout()->associate($layout);
+
+        $this->actingAs(User::factory()->create()->assignRole('Admin'));
+
+        $resource = new \DrewRoberts\Blog\Nova\Post($posts);
+        $request = NovaRequest::create('posts');
+        $resource::relatableLayouts($request, $posts);
+
+        $this->assertCount(1, $resource->indexFields($request)->where('attribute', 'layout'));
     }
 }

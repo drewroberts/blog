2959b09d1016bdcfcb6626ce4e8bd19b4eff9dee
ryzhykE <eugene@buildmidwestern.com>
2021-05-17 16:04:41 +0300

    Create relatableLayouts test for SeriesResource #17

--- tests/Feature/Nova/SeriesResourceTest.php
+++ tests/Feature/Nova/SeriesResourceTest.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace DrewRoberts\Blog\Tests\Feature\Nova;
 
+use DrewRoberts\Blog\Models\Layout;
 use DrewRoberts\Blog\Models\Series;
 use DrewRoberts\Blog\Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Laravel\Nova\Http\Requests\NovaRequest;
 use Tipoff\Authorization\Models\User;
 
 class SeriesResourceTest extends TestCase
@@ -38,5 +40,20 @@ class SeriesResourceTest extends TestCase
             ->assertOk();
 
         $this->assertEquals($series->id, $response->json('resource.id.value'));
+    }
+
+    /** @test */
+    public function relatableLayouts()
+    {
+        $layout = Layout::factory()->create();
+        $series = Series::factory()->create()->layout()->associate($layout);
+
+        $this->actingAs(User::factory()->create()->assignRole('Admin'));
+
+        $resource = new \DrewRoberts\Blog\Nova\Series($series);
+        $request = NovaRequest::create('series');
+        $resource::relatableLayouts($request, $series);
+
+        $this->assertCount(1, $resource->indexFields($request)->where('attribute', 'layout'));
     }
 }
